0d9fab3fbec721e278e64a4badb947e13cbccab5
krokith <krokith20@gmail.com>
2024-12-26 22:58:01 +0530

    first commit

---
+++ src/constructor.java
@@ -0,0 +1,20 @@
+public class constructor {
+        public static void main(String[] args) {
+            int a = 100;
+            int b = 50;
+            int c = 20;
+            int d = 10;
+
+            int sum = a+b;
+            int sub = a-b;
+            int mul = a*b;
+            int div = a/b;
+
+            System.out.println("Addition : "+sum);
+            System.out.println("Addition : "+sub);
+            System.out.println("Addition : "+mul);
+            System.out.println("Addition : "+div);
+        }
+    }
+
+
